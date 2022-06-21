f436669433e91b19e4153f6f447903e6737a507b
Vinicius Oliveira <vinicius.shk@gmail.com>
2022-06-21 16:21:56 -0300

    Conclusão do requisito 7

---
+++ src/components/Missions.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Missions extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        <p>Req 7 test</p>
+      </div>
+    );
+  }
+}
+
+export default Missions;
