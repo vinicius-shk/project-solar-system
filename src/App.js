ae1d6e589d776f99fed1fd2c82a85288521b5943
Vinicius Oliveira <vinicius.shk@gmail.com>
2023-01-17 13:13:27 -0300

    reponsive CSS

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ import './App.css';
 class App extends React.Component {
   render() {
     return (
-      <>
+      <main>
         <Header />
         <SolarSystem />
         <Missions />
-      </>
+      </main>
     );
   }
 }
